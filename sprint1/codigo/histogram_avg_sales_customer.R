154c58e163725f5f2304a36db9abb42b8d3fd1c8
crilobra <crilobra@gmail.com>
2024-08-18 19:43:02 -0400

    Add files via upload

--- sprint1/codigo/histogram_avg_sales_customer.R
+++ sprint1/codigo/histogram_avg_sales_customer.R
@@ -1,4 +1,4 @@
-# Primero indicamos la ruta a la carpeta de de tu computadora 
+# Primero indicamos la ruta a la carpeta de de tu computadora
 # donde se ubican los datos del E-commerce
 # Ejemplo: "C:\Usuarios\[tu nombre]\Descargas"
 
@@ -7,69 +7,73 @@ library(readxl)
 library(lubridate)
 library(corrplot)
 
-DATA_PATH="/Users/Christian/Documents/unifranz/nao/R/s1/"
+datapath <- "/Users/Christian/Documents/unifranz/nao/R/s1/"
 
 # Establece el directorio de trabajo
-setwd(DATA_PATH)
-SALES_DATA = './data/sales.csv'
+setwd(datapath)
+salesdata <- "./data/sales.csv"
 
 # Datos adicionales de tiendas
-STORES_INFO_DATA = './data/stores_info.xlsx'
+storesinfodata <- "./data/stores_info.xlsx"
 sales <- read.csv2(
-  SALES_DATA,
-  sep=";",
-  header=T
+  salesdata,
+  sep = ";",
+  header = TRUE
 )
 
 sales %>% tail()
 # Completa el codigo para leer la
 stores <- read_excel(
-  STORES_INFO_DATA,
+  storesinfodata,
   sheet = "data" # nombre de la hoja del archivo Excel
 )
 
 
 # Une las tablas
-sales_all <- sales %>% left_join(stores, by=c("store"))
+sales_all <- sales %>% left_join(stores, by = c("store"))
 
 # Muestra la ultimas 10 filas
 sales_all %>% tail(10)
 
 
 # Une las tablas
-sales_all  %>% 
+sales_all  %>%
   group_by(country) %>%
   summarise(
     # calculo de ventas en millones
-    sales = sum(sales)/1000000,
+    sales = sum(sales) / 1000000,
     # calculo de customers en millones
-    customers = sum(customers)/1000000
+    customers = sum(customers) / 1000000
   )
 
 # convierte a fecha la columna
 sales$date <- ymd(sales$date)
 
 # extrae el año de la fecha de venta
-sales_all <- sales_all %>% mutate(year = as.numeric(format(as.Date(sales_all$date), "%Y")))
-# Extrae al mes de la fecha de venta 
-sales_all <- sales_all %>% mutate(month = as.numeric(format(as.Date(sales_all$date), "%m")))
-# Extrae al dia de la fecha de venta 
-sales_all <- sales_all %>% mutate(day = as.numeric(format(as.Date(sales_all$date), "%d")))
+sales_all <- sales_all %>%
+  mutate(year = as.numeric(format(as.Date(sales_all$date), "%Y")))
+# Extrae al mes de la fecha de venta
+sales_all <- sales_all %>%
+  mutate(month = as.numeric(format(as.Date(sales_all$date), "%m")))
+# Extrae al dia de la fecha de venta
+sales_all <- sales_all %>%
+  mutate(day = as.numeric(format(as.Date(sales_all$date), "%d")))
 # Cre una fecha auxiliar para el analisis (date_custom)
 sales_all <- sales_all %>% mutate(day = 1)
-sales_all <- sales_all %>% mutate('date_custom' = ISOdate(year = year, month = month, day = day))
-# Extrae al dia de la fecha de venta 
-sales_all <- sales_all %>% mutate(yearmonth = as.numeric(format(as.Date(sales_all$date), "%Y%m")))
+sales_all <- sales_all %>%
+  mutate("date_custom" = ISOdate(year = year, month = month, day = day))
+# Extrae al dia de la fecha de venta
+sales_all <- sales_all %>%
+  mutate(yearmonth = as.numeric(format(as.Date(sales_all$date), "%Y%m")))
 
-sales_all <- sales_all %>% filter(country != 'italy')
-  
-  
-avg_sales <- sales_all  %>% 
+sales_all <- sales_all %>% filter(country != "italy")
+
+avg_sales <- sales_all  %>%
   group_by(country, year) %>%
   summarise(
     # calculo de ventas en customers
-    avg_sales_customes = sum(sales)/sum(customers)
-  ) 
+    avg_sales_customes = sum(sales) / sum(customers)
+  )
 
 # Calcula promedio and  desviation estandar de las ventas
 mean_sales <- mean(avg_sales$avg_sales_customes)
@@ -81,7 +85,11 @@ ggplot(avg_sales, aes(x = avg_sales_customes)) +
   # Agrega una linea en el promedio
   geom_vline(xintercept = mean_sales, linetype = "dashed", color = "blue") +
   # Agrega una linea en el promedio mas 2 desviaciones estandar
-  geom_vline(xintercept = mean_sales + 2 * std_dev_sales, linetype = "dashed", color = "red") +
+  geom_vline(xintercept = mean_sales + 2 * std_dev_sales,
+    linetype = "dashed", color = "red"
+  ) +
   # Agrega una linea en el promedio menos 2 desviaciones estandar
-  geom_vline(xintercept = mean_sales - 2 * std_dev_sales, linetype = "dashed", color = "red")+
+  geom_vline(xintercept = mean_sales - 2 * std_dev_sales,
+    linetype = "dashed", color = "red"
+  ) +
   labs(title = "Histograma de Ventas y la regla empírica débil")
--- sprint1/codigo/prop_changes_sales_by_country_by_year_month.R
+++ sprint1/codigo/prop_changes_sales_by_country_by_year_month.R
@@ -1,4 +1,4 @@
-# Primero indicamos la ruta a la carpeta de de tu computadora 
+# Primero indicamos la ruta a la carpeta de de tu computadora
 # donde se ubican los datos del E-commerce
 # Ejemplo: "C:\Usuarios\[tu nombre]\Descargas"
 
@@ -7,77 +7,84 @@ library(readxl)
 library(lubridate)
 library(corrplot)
 
-DATA_PATH="/Users/Christian/Documents/unifranz/nao/R/s1/"
+datapath <- "/Users/Christian/Documents/unifranz/nao/R/s1/"
 
 # Establece el directorio de trabajo
-setwd(DATA_PATH)
-SALES_DATA = './data/sales.csv'
+setwd(datapath)
+salesdata <- "./data/sales.csv"
 
 # Datos adicionales de tiendas
-STORES_INFO_DATA = './data/stores_info.xlsx'
+storesinfodata <- "./data/stores_info.xlsx"
 sales <- read.csv2(
-  SALES_DATA,
-  sep=";",
-  header=T
+  salesdata,
+  sep = ";",
+  header = TRUE
 )
 
 sales %>% tail()
 # Completa el codigo para leer la
 stores <- read_excel(
-  STORES_INFO_DATA,
+  storesinfodata,
   sheet = "data" # nombre de la hoja del archivo Excel
 )
 view(sales)
 
 # Une las tablas
-sales_all <- sales %>% left_join(stores, by=c("store"))
+sales_all <- sales %>% left_join(stores, by = c("store"))
 
 # Muestra la ultimas 10 filas
 sales_all %>% tail(10)
 
 
 # Une las tablas
-sales_all  %>% 
+sales_all  %>%
   group_by(country) %>%
   summarise(
     # calculo de ventas en millones
-    sales = sum(sales)/1000000,
+    sales = sum(sales) / 1000000,
     # calculo de customers en millones
-    customers = sum(customers)/1000000
+    customers = sum(customers) / 1000000
   )
 
 # convierte a fecha la columna
 sales$date <- ymd(sales$date)
 
 # extrae el año de la fecha de venta
-sales_all <- sales_all %>% mutate(year = as.numeric(format(as.Date(sales_all$date), "%Y")))
-# Extrae al mes de la fecha de venta 
-sales_all <- sales_all %>% mutate(month = as.numeric(format(as.Date(sales_all$date), "%m")))
-# Extrae al dia de la fecha de venta 
-sales_all <- sales_all %>% mutate(day = as.numeric(format(as.Date(sales_all$date), "%d")))
+sales_all <- sales_all %>%
+  mutate(year = as.numeric(format(as.Date(sales_all$date), "%Y")))
+# Extrae al mes de la fecha de venta
+sales_all <- sales_all %>%
+  mutate(month = as.numeric(format(as.Date(sales_all$date), "%m")))
+# Extrae al dia de la fecha de venta
+sales_all <- sales_all %>%
+  mutate(day = as.numeric(format(as.Date(sales_all$date), "%d")))
 # Cre una fecha auxiliar para el analisis (date_custom)
 sales_all <- sales_all %>% mutate(day = 1)
-sales_all <- sales_all %>% mutate('date_custom' = ISOdate(year = year, month = month, day = day))
-# Extrae al dia de la fecha de venta 
-sales_all <- sales_all %>% mutate(yearmonth = as.numeric(format(as.Date(sales_all$date), "%Y%m")))
+sales_all <- sales_all %>%
+  mutate("date_custom" = ISOdate(year = year, month = month, day = day))
+# Extrae al dia de la fecha de venta
+sales_all <- sales_all %>%
+  mutate(yearmonth = as.numeric(format(as.Date(sales_all$date), "%Y%m")))
 
 
-prop_changes_sales_by_country_by_year_month <- sales_all  %>% 
+prop_changes_sales_month <- sales_all  %>%
   group_by(country, yearmonth) %>%
   summarise(
     # calculo de ventas en customers
     sales = sum(sales)
   ) %>%
-  ungroup()%>%
+  ungroup() %>%
   group_by(yearmonth) %>%
-  mutate(sales = sales / sum(sales) * 100)%>%
+  mutate(sales = sales / sum(sales) * 100) %>%
   # ungroup para obtener un dataframe sin grupos
-ungroup() %>% 
+  ungroup() %>%
   # Crea la estructura de la pivot alargando la tabla
   pivot_wider(
     names_from = yearmonth,
-    values_from=sales
+    values_from = sales
   )
 
-
-write.csv(prop_changes_sales_by_country_by_year_month,'prop_changes_sales_by_country_by_year_month.csv',row.names =TRUE)
+write.csv(
+  prop_changes_sales_month,
+  "prop_changes_sales_by_country_by_year_month.csv", row.names = TRUE
+)
